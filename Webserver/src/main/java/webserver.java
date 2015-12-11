6dde12ea502cdfe10f13e44dfc3b3af4f47ba483
josephrdean <josephrdean@gmail.com>
2015-12-11 11:44:56 -0800

    Updated .gitignore, seems to work great
    Updated .gitignore, seems to work great

---
+++ Webserver/src/main/java/webserver.java
@@ -0,0 +1,32 @@
+// Basic webserver example copied from
+// http://stackoverflow.com/questions/3732109/simple-http-server-in-java-using-only-java-se-api
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+// A webserver that implements a single POST endpoint; watch_and_notify
+public class webserver {
+
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/watch_and_notify", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            // Need to validate the request is a POST. That's the only verb we accept.
+            String response = "This is the response";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
